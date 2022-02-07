e45ac4ef75c033c7fad2eeada42c3437d62a710d
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:14 -0500

    Set default language to spanish.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
   private
   
   def set_default_language
-    I18n.locale = request.headers['HTTP_ACCEPT_LANGUAGE'].split(',')[0].split('-')[0] rescue 'es'
+    I18n.locale = 'es'#request.headers['HTTP_ACCEPT_LANGUAGE'].split(',')[0].split('-')[0] rescue 'es'
   end
 end
