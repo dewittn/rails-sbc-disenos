1c886678cbf5285734a6c80647566005a1d2ff1e
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:10 -0500

    Unobtrusive Ajax search

---
+++ public/javascripts/disenos_index.js
@@ -0,0 +1,13 @@
+Event.addBehavior({
+  '#search': function(e){
+    new Form.Element.Observer('search', 0.5, function(element, value){
+      new Ajax.Updater('results', '/disenos.js', {
+        asynchronous:true, evalScripts:true, method:'get', parameters:'search=' + element.value
+        })
+      })
+    },
+  
+  'input[type=submit]': function(){
+    this.hide();
+  }
+});
