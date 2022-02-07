73edd3f04751c469d5e91a5c015b80e11cec912f
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:10 -0500

    Tweaks to Diseno/new to get the marcas and colors to display correctly. Validation errors and message

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -4,11 +4,14 @@ class Diseno < ActiveRecord::Base
   has_attached_file :archivo_dst
   has_attached_file :archivo_pes
   
+  validates_presence_of :nombre_de_orden
+  validates_presence_of :cantidad_del_colores
+  
   accepts_nested_attributes_for :hilos
 
   define_index do
     indexes nombre_de_orden, :sorable => true
     
-    set_property :delta => true
+    #set_property :delta => true
   end
 end
--- app/models/hilo.rb
+++ app/models/hilo.rb
@@ -1,4 +1,14 @@
 class Hilo < ActiveRecord::Base
   belongs_to :diseno
   belongs_to :color
+  
+  validates_presence_of :color_id, :message => "faltan"
+  
+  def marca_id
+    @marca_id
+  end
+  
+  def marca_id=(id)
+    @marca_id = (id.to_i rescue 0)
+  end
 end
