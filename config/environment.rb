0594b74f8a9ccceee3537a0a77482124a78bcd4d
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:09 -0500

    Added gem requirements

--- config/environment.rb
+++ config/environment.rb
@@ -41,6 +41,9 @@ Rails::Initializer.run do |config|
     :secret      => 'db2906790813e6dd612817c0a4690fbc6f88e1532dd181a5b86eadd3fded3a347dcc4d18c35df62a3cbeab48bfd098805c2615eaacf13d7516b486d0121d1d65'
   }
 
+  config.gem "mysql"
+  config.gem "thoughtbot-paperclip", :lib => "paperclip"
+
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
   # (create the session table with 'rake db:sessions:create')
