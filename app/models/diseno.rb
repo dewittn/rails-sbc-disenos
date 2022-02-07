fc65870375a73052ee719bfdeaac300e7a88f4e3
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Fixed relative_root problem

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -1,8 +1,8 @@
 class Diseno < ActiveRecord::Base
   has_many :hilos, :dependent => :destroy
-  has_attached_file :image, :url => relative_root
-  has_attached_file :archivo_dst, :url => relative_root
-  has_attached_file :archivo_pes, :url => relative_root
+  has_attached_file :image, :url => :relative_root
+  has_attached_file :archivo_dst, :url => :relative_root
+  has_attached_file :archivo_pes, :url => :relative_root
   
   validates_presence_of :nombre_de_orden
   validates_presence_of :cantidad_del_colores
