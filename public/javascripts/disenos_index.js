fba97aa5fe3c537a32b07a16206b2a305b60c381
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Added path_prefix for JS. Changed path for ajax requests

--- public/javascripts/disenos_index.js
+++ public/javascripts/disenos_index.js
@@ -1,7 +1,7 @@
 Event.addBehavior({
   '#search': function(e){
     new Form.Element.Observer('search', 0.5, function(element, value){
-      new Ajax.Updater('results', './disenos.js', {
+      new Ajax.Updater('results', path_prefix + '/disenos.js', {
         asynchronous:true, evalScripts:true, method:'get', parameters:'search=' + element.value
         })
       })
