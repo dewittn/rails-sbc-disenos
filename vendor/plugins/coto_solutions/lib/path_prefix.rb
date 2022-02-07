4606eff1e918e1da92f4c9b9c4bf8e7173c60604
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Added rack PathPrefix

---
+++ vendor/plugins/coto_solutions/lib/path_prefix.rb
@@ -0,0 +1,29 @@
+class PathPrefix
+  # :api: private
+  def initialize(app, path_prefix = nil)
+    @app = app
+    @path_prefix = /^#{Regexp.escape(path_prefix)}/
+  end
+  
+  # :api: plugin
+  def deferred?(env)
+    strip_path_prefix(env) 
+    @app.deferred?(env)
+  end
+  
+  # :api: plugin
+  def call(env)
+    strip_path_prefix(env) 
+    @app.call(env)
+  end
+
+  # :api: private
+  def strip_path_prefix(env)
+    ['PATH_INFO', 'REQUEST_URI'].each do |path_key|
+      if env[path_key] =~ @path_prefix
+        env[path_key].sub!(@path_prefix, "")
+        env[path_key] = "/" if env[path_key].empty?
+      end
+    end
+  end
+end
