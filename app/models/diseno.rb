088b2990fddffb7f8fec57a1c0f67170cba6843e
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Removed relative_root method

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -1,17 +1,13 @@
 class Diseno < ActiveRecord::Base
   has_many :hilos, :dependent => :destroy
-  has_attached_file :image, :url => :relative_root
-  has_attached_file :archivo_dst, :url => :relative_root
-  has_attached_file :archivo_pes, :url => :relative_root
+  has_attached_file :image, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:filename"
+  has_attached_file :archivo_dst, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:filename"
+  has_attached_file :archivo_pes, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:filename"
   
   validates_presence_of :nombre_de_orden
   validates_presence_of :cantidad_del_colores
   
   accepts_nested_attributes_for :hilos, :allow_destroy => true
-  
-  def relative_root
-    @relative_root ||= "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:filename"
-  end
 
   define_index do
     indexes nombre_de_orden, :sorable => true
