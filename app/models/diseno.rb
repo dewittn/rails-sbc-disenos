bf43e6a074f2ef2da4ef534cae768e927335efcb
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:13 -0500

    Added orignal image and notes to design new/edit views. Added delete button and action to design edit.

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -1,11 +1,21 @@
 class Diseno < ActiveRecord::Base
   has_many :hilos, :dependent => :destroy
-  has_attached_file :image, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension", :styles => { :medium => "300x300>"}
-  has_attached_file :archivo_dst, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
-  has_attached_file :archivo_pes, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
+  has_attached_file :image, 
+                    :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", 
+                    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension", 
+                    :styles => { :medium => "300x300>", :small => "100x100" }
+  has_attached_file :original, 
+                    :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", 
+                    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension", 
+                    :styles => { :medium => "300x300>" }
+  has_attached_file :archivo_dst, 
+                    :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", 
+                    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
+  has_attached_file :archivo_pes, 
+                    :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", 
+                    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   
   validates_presence_of :nombre_de_orden
-  # validates_presence_of :cantidad_del_colores
   
   accepts_nested_attributes_for :hilos, :allow_destroy => true
   
