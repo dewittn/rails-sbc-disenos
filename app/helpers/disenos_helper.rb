c247f889bf3b109ab7f5e9eb1f6ffc1b2c79f78b
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:12 -0500

    Added helper to show picture if small image is not available.

--- app/helpers/disenos_helper.rb
+++ app/helpers/disenos_helper.rb
@@ -2,4 +2,8 @@ module DisenosHelper
   def diseno_nombre
     @diseno.nombre_de_orden.blank? ? "No Tenia Nombre" : h(@diseno.nombre_de_orden)
   end
+  
+  def image_url(diseno)
+    @diseno.image.url(:medium) ? @diseno.image.url(:medium) : @diseno.image.url
+  end
 end
