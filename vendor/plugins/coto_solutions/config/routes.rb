72a7800e3693ba026bd63ee1ca3f9f3922e7177f
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    adjustment to coto_solutions route file.Overwriting app javascipts route

--- vendor/plugins/coto_solutions/config/routes.rb
+++ vendor/plugins/coto_solutions/config/routes.rb
@@ -1,3 +1,3 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :javascripts, :collection => { :path_prefix => :get }
+  map.resources :javascripts, :collection => { :path_prefix => :get }, :only => :path_prefix
 end
