c427039fc0ef601289986a498db69d44418cd62d
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Added Javascripts controller to colo_solutions plugin

---
+++ vendor/plugins/coto_solutions/app/controllers/javascripts.rb
@@ -0,0 +1,6 @@
+class JavascriptsController < ApplicationController
+  caches_page :path_prefix
+  
+  def path_prefix
+  end
+end
--- vendor/plugins/coto_solutions/init.rb
+++ vendor/plugins/coto_solutions/init.rb
@@ -1,3 +1,2 @@
 # Include hook code here
 require "coto_solutions"
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion(:coto_solutions => ["path_prefix"])
