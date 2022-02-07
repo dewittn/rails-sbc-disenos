3992e9fd947fb3a569c13e48f73909e4afd6d37d
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:13 -0500

    Moved search to top bar for global search.

--- public/javascripts/disenos_index.js
+++ public/javascripts/disenos_index.js
@@ -1,12 +1,4 @@
 Event.addBehavior({
-  '#search': function(e){
-    new Form.Element.Observer('search', 0.5, function(element, value){
-      new Ajax.Updater('results', path_prefix + '/disenos.js', {
-        asynchronous:true, evalScripts:true, method:'get', parameters:'search=' + element.value
-        })
-      })
-    },
-  
   'input[type=submit]': function(){
     this.hide();
   },
