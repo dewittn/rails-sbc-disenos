5da363e68ad357dc3ce74054c8a3ead64ed3a747
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:13 -0500

    Increased size of convertage picture

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -1,6 +1,6 @@
 class Diseno < ActiveRecord::Base
   has_many :hilos, :dependent => :destroy
-  has_attached_file :image, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension", :styles => { :medium => "200x200>"}
+  has_attached_file :image, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension", :styles => { :medium => "300x300>"}
   has_attached_file :archivo_dst, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   has_attached_file :archivo_pes, :url => "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:basename.:extension", :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
   
