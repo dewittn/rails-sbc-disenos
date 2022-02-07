3348e17cc409c68497f6d1e2a7ce88e1ca111621
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Fixed register_javascript_expansion code to be array

--- vendor/plugins/coto_solutions/init.rb
+++ vendor/plugins/coto_solutions/init.rb
@@ -1,5 +1,5 @@
 # Include hook code here
 require "coto_solutions"
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion(
-  :path_prefix => "path_prefix"
+  :coto_solutions => ["path_prefix"]
 )
