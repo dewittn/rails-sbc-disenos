83d909ced9fb232ee399a2c4d49008794e79f54e
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:14 -0500

    Changes to sweeper to address bug.

--- app/controllers/disenos_controller.rb
+++ app/controllers/disenos_controller.rb
@@ -1,5 +1,5 @@
 class DisenosController < ApplicationController
-  cache_sweeper :diseno_sweeper, :only => [:update, :destory, :create]
+  cache_sweeper :diseno_sweeper, :only => [:update, :destory]
   caches_page :index, :new, :show, :if => Proc.new { |c| !c.request.format.js? }
   
   def index
