b4beae7bb0c85efadc434e4194d16dbd9f41eb71
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:08 -0500

    Hilos update

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -1,7 +1,7 @@
 class Diseno < ActiveRecord::Base
-  file_column :image
-  file_column :archivo_dst
-  file_column :archivo_pes
+  has_attached_file :image
+  has_attached_file :archivo_dst
+  has_attached_file :archivo_pes
   has_many :hilos
   
   def hilo_attributes=(hilo_attributes)
---
+++ db/migrate/006_paper_clip_image_cols.rb
@@ -0,0 +1,15 @@
+class PaperClipImageCols < ActiveRecord::Migration
+  def self.up
+    add_column :disenos, :image_file_name, :string
+    add_column :disenos, :image_content_type, :string
+    add_column :disenos, :image__file_size, :string
+    remove_column :disenos, :image
+  end
+
+  def self.down
+    add_column :disenos, :image, :string
+    remove_column :disenos, :image__file_size
+    remove_column :disenos, :image_content_type
+    remove_column :disenos, :image_file_name
+  end
+end
