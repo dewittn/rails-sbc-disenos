80c7362002669234084000d67fb9af7636e8dc94
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:13 -0500

    Colors can be added and removed from the hilos#new form.

--- app/models/marca.rb
+++ app/models/marca.rb
@@ -1,5 +1,6 @@
 class Marca < ActiveRecord::Base
   acts_as_cached
   has_many :colors
-  accepts_nested_attributes_for :colors, :allow_destroy => true, :reject_if => proc { |attributes| attributes['nombre'].blank? }
+  validates_presence_of :nombre
+  accepts_nested_attributes_for :colors, :allow_destroy => true#, :reject_if => proc { |attributes| attributes['nombre'].blank? }
 end
