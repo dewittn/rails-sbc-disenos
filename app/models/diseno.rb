c7c0de1ea0f17fe030cc675cfd658057dded2c1a
Nelson/Roberto <git@nelsonroberto.com>
2022-02-07 16:32:11 -0500

    Added relative root to paperclip

--- app/models/diseno.rb
+++ app/models/diseno.rb
@@ -1,8 +1,8 @@
 class Diseno < ActiveRecord::Base
   has_many :hilos, :dependent => :destroy
-  has_attached_file :image
-  has_attached_file :archivo_dst
-  has_attached_file :archivo_pes
+  has_attached_file :image, :url => relative_root
+  has_attached_file :archivo_dst, :url => relative_root
+  has_attached_file :archivo_pes, :url => relative_root
   
   validates_presence_of :nombre_de_orden
   validates_presence_of :cantidad_del_colores
@@ -13,5 +13,9 @@ class Diseno < ActiveRecord::Base
     indexes nombre_de_orden, :sorable => true
     
     set_property :delta => true
+  end
+  
+  def relative_root
+    @relative_root ||= "#{ActionController::Base.relative_url_root.to_s}/system/:attachment/:id/:style/:filename"
   end
 end
